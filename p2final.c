c7c6f19fe1524ec81979859f1afa1aa0d65d9ba1
Yatham Tejasri <94678025+Sridha123@users.noreply.github.com>
2022-03-27 10:15:14 +0000

    p2final.c

--- p2final.c
+++ p2final.c
@@ -5,22 +5,22 @@ int input()
   printf("enter a number\n");
   scanf("%d",&a);
   return a;
-}
-int cmp(int a,int b, int c)
+  }
+int cmp(int a,int b,int c)
 {
   int l;
   if((a>b)&&(a>c))
-  printf("A  is big\n");
+   printf("A is big\n");
   else if((b>a)&&(b>c))
-  printf("B is big\n");
+    printf("B is big\n");
   else if((c>a)&&(c>b))
-  printf("C is big\n");
+    printf("c is big\n");
   return 0;
-}
+  }
 void output(int a,int b,int l)
 {
   printf("largest number is %d\n",l);
-}
+  }
 int main()
 {
   int a,b,c,l;
@@ -29,9 +29,5 @@ int main()
   c=input();
   l=cmp(a,b,c);
   return 0;
-}
-
-
-
-
+  }
 
