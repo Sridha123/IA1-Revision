e5123f6d9dace464d1c767229b5e6e257e1529a9
Yatham Tejasri <94678025+Sridha123@users.noreply.github.com>
2022-03-27 09:58:37 +0000

    p1final.c

--- p1final.c
+++ p1final.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 void input(int *a,int *b)
 {
-printf("Enter two values\n");
-scanf("%d %d",a,b);
-}
+  printf("Enter two values\n");
+  scanf("%d %d",a,b);
+  }
 void add(int a,int b,int*sum)
 {
-*sum=a+b;
-}
+  *sum=a+b;
+  }
 void output(int a,int b,int sum)
 {
-  printf("the sum of %d + %d is %d\n",a,b,sum);
-}
+  printf("the sum of %d + %d is %d/n",a,b,sum);
+  }
 int main()
 {
   int a,b,sum;
   input(&a,&b);
   add(a,b,&sum);
-output(a,b,sum);
-return 0;
-}
+  output(a,b,sum);
+  return 0;
+  }
 
 
